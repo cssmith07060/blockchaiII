161c91ed90b03c5d290697155e99e478fb5bf99a
cssmith07060 <cssmith07060@gmail.com>
2022-09-01 06:23:19 -0400

    first commit

---
+++ blockchainII.py
@@ -0,0 +1,19 @@
+class Block:
+    '''
+Block: a unit of storage.
+Store transactions in a blockchain that supports a cryptocurrency.
+'''
+def__init__(self):
+    self.data = data
+    
+    
+class Blockchain:
+    '''
+    Blockchain: a public ledger of transactions.
+    Implemented as a list of blocks - data sets of transactions
+    '''
+    
+    def__init__(self):
+        self.chain = []
+        
+         def add_block()
