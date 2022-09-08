d55d274d3994e7254e52e3486f0dd4c12205c42e
cssmith07060 <cssmith07060@gmail.com>
2022-09-07 23:26:46 -0400

    running man

--- block.py
+++ block.py
@@ -1,12 +1,29 @@
+def mine_block(last_block, data):
+     """
+     Mine a block based on the given last_block and data.
+     """
+
+
 class Block:
     '''
 Block: a unit of storage.
 Store transactions in a blockchain that supports a cryptocurrency.
 '''
-def__init__(self):
+def__init__(self, timestamp, last_hash, hash):
+    self.timestamp= timestamp
+    self.last_hash = last_hash
+    self.hash = hash
     self.data = data
 def __repr__(self):    
-    return f 'Block: {self.data}'
+     return (
+          'Block('
+          f'Block: {self.data},'
+          f'last_hash: {self.last_hash},'
+          f'hash: {hash},'
+          f'data: {self.data})'
+          
+          
+     )
 def main():
      block = Block('foo')
      print(block)
