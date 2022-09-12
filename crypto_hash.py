5437f11e89b48eece9389f93ccdf6ce4e051df6c
cssmith07060 <cssmith07060@gmail.com>
2022-09-12 07:15:18 -0400

    next

--- crypto_hash.py
+++ crypto_hash.py
@@ -1,15 +1,24 @@
 import hashlib
 import json
+
+def stringify(data):
+    return json.dumps(data)
+
+
 def crypto_hash(*args):
     """
     Return a sha-256 hash of the given data.
     """
     
-    stringified_data = json.dumps(data)
+    stringified_args = map(, args)
     
-    return hashlib.sha256(stringified_data.encode('utf-8')).hexdigest()
+    joined_data = ''.join(args)
+    print(f'joined_data: {joined_data}')
+    
+    return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 def main():
+    print(f "crypto_hash('one', 'two', 'three') : {crypto_hash({'one', 'two', 'three')})
     
 if__name__=='__main__':
         main()
