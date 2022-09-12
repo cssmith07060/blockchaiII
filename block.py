c439eb1d18637611b89a4715be280c5fdf85fb83
cssmith07060 <cssmith07060@gmail.com>
2022-09-11 20:27:54 -0400

    special ed

--- block.py
+++ block.py
@@ -33,9 +33,28 @@ def __repr__(self):
           f'last_hash: {self.last_hash},'
           f'hash: {hash},'
           f'data: {self.data})'
-          
-          
-     )
+          )
+     
+@staticmethod     
+def mine_block(last_block, data):
+         """
+     Mine a block based on the given last_block and data.
+     """
+
+   timestamp = time.time_ns()
+   last_hash = last_block.hash
+   hash = f '{timestamp} - {last_hash}'
+   
+   return Block(timestamp, last_hash, hash, data)
+
+@staticmethod
+def genesis():
+     '''
+     Generate the genesis block.
+     '''
+     return Block(1, 'genesis_last_hash', 'genesis_hash', [])
+
+class Block:
 def main():
      block = Block('foo')
      print(block)
--- blockchainII.py
+++ blockchainII.py
@@ -1,4 +1,4 @@
-from block import Block
+from block import Block, genesis, mine_block
 class Blockchain:
     '''
     Blockchain: a public ledger of transactions.
@@ -6,10 +6,10 @@ class Blockchain:
     '''
     
     def__init__(self):
-        self.chain = []
+        self.chain = [genesis()]
         
          def add_block(self, data):
-             self.chain.append(Block(data))
+            self.chain.append(mine_block(self.chain[-1], data))
              
          def __repr__(self):
              return f 'Blockchain: {self.chain}'
